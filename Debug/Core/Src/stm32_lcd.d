2ac6a7966a56924463d9489ff508a12071819917
smoki599 <69309225+smoki599@users.noreply.github.com>
2023-06-26 15:39:44 +0200

    Add files via upload

---
+++ Debug/Core/Src/stm32_lcd.d
@@ -0,0 +1,17 @@
+Core/Src/stm32_lcd.o: ../Core/Src/stm32_lcd.c ../Core/Inc/stm32_lcd.h \
+ ../Core/Inc/fonts.h \
+ /home/samo/Downloads/2/Stm32H750B-DK_Minesweeper-main\ (1)/Piano/Drivers/BSP/Components/Common/lcd.h \
+ ../Core/Src/../../Utilities/Fonts/font24.c ../Core/Inc/fonts.h \
+ ../Core/Src/../../Utilities/Fonts/font20.c \
+ ../Core/Src/../../Utilities/Fonts/font16.c \
+ ../Core/Src/../../Utilities/Fonts/font12.c \
+ ../Core/Src/../../Utilities/Fonts/font8.c
+../Core/Inc/stm32_lcd.h:
+../Core/Inc/fonts.h:
+/home/samo/Downloads/2/Stm32H750B-DK_Minesweeper-main\ (1)/Piano/Drivers/BSP/Components/Common/lcd.h:
+../Core/Src/../../Utilities/Fonts/font24.c:
+../Core/Inc/fonts.h:
+../Core/Src/../../Utilities/Fonts/font20.c:
+../Core/Src/../../Utilities/Fonts/font16.c:
+../Core/Src/../../Utilities/Fonts/font12.c:
+../Core/Src/../../Utilities/Fonts/font8.c:
